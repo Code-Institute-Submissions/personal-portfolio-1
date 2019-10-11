4ab48568771596895040233d7fa80edbbe8d5b16
jomari <jomarialang.com>
2019-10-11 13:56:04 +0100

    added mobile navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
+	globalListener();
+
 	onHoverProjects();
+	onScrollPortrait();
+	onClickMenuIcon();
+	onClickMobileLinks();
 });
+
 function onHoverProjects() {
 	var projects = $("#project .project-item");
 
@@ -12,4 +18,38 @@ function onHoverProjects() {
 			$(this).removeClass("active");
 		}
 	);
+}
+
+function onScrollPortrait() {
+	var target = $("#hero .portrait");
+	var initPos = target.offset().top;
+	$(window).scroll(function(event) {
+		var yCoor = $(this).scrollTop();
+		target.css("transform", `translateY(${yCoor * 0.3 + initPos}px)`);
+	});
+}
+
+function onClickMenuIcon() {
+	var trigger = $("header .menu-icon");
+	var target = $(".mobile-nav");
+	trigger.click(function() {
+		$(this).toggleClass("active");
+		target.toggleClass("active");
+	});
+}
+
+function onClickMobileLinks() {
+	var trigger = $(".mobile-nav .mobile-link");
+	var target = $(".mobile-nav");
+
+	trigger.click(function() {
+		target.removeClass("active");
+	});
+}
+
+function globalListener() {
+	var target = $(".mobile-nav");
+	$(window).scroll(function() {
+		target.removeClass("active");
+	});
 }
